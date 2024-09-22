1ee40615cbc766e25b383cd6c24169bbaecaf720
rafaeljuniora <160675357+rafaeljuniora@users.noreply.github.com>
2024-09-21 22:07:07 -0300

    Update Endereco.java

--- Aula_06/src/Trabalho_Aula_06/Endereco.java
+++ Aula_06/src/Trabalho_Aula_06/Endereco.java
@@ -1,12 +1,10 @@
 package Trabalho_Aula_06;
-
 public class Endereco {
     private String rua;
     private String cidade;
     private String numero;
     private String cep;
     private boolean enderecoAtual;
-
     public Endereco(boolean enderecoAtual, String cep, String numero, String rua, String cidade) {
         this.enderecoAtual = enderecoAtual;
         this.cep = cep;
@@ -14,43 +12,35 @@ public class Endereco {
         this.rua = rua;
         this.cidade = cidade;
     }
-
     public String getRua() {
         return rua;
     }
-
     public String getCidade() {
         return cidade;
     }
-
     public String getNumero() {
         return numero;
     }
-
     public String getCep() {
         return cep;
     }
-
     public boolean isEnderecoAtual() {
         return enderecoAtual;
     }
-
     public void setRua(String rua) {
         this.rua = rua;
     }
-
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
-
+    
     public void setNumero(String numero) {
         this.numero = numero;
     }
-
     public void setCep(String cep) {
         this.cep = cep;
     }
-
+    
     public void setEnderecoAtual(boolean enderecoAtual) {
         this.enderecoAtual = enderecoAtual;
     }
