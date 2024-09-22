13d9f0efd2df7ebf64449be9ae99ee8e1a86ff49
rafaeljuniora <160675357+rafaeljuniora@users.noreply.github.com>
2024-09-21 22:06:26 -0300

    Update Cliente.java

--- Aula_06/src/Trabalho_Aula_06/Cliente.java
+++ Aula_06/src/Trabalho_Aula_06/Cliente.java
@@ -1,14 +1,11 @@
 package Trabalho_Aula_06;
-
 public class Cliente {
     private String nome;
     private Endereco endereco;
-
     public Cliente(Endereco enderecos, String nome) {
         this.endereco = enderecos;
         this.nome = nome;
     }
-
     public String getNome() {
         return nome;
     }
@@ -16,10 +13,8 @@ public class Cliente {
     public Endereco getEndereco() {
         return endereco;
     }
-
     @Override
     public String toString() {
         return nome +", Endereço: "+ endereco;
-
     }
 }
